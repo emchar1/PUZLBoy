e5199e311f2383da53fc7d593e75a9a077535a4a
Eddie Char <emchar1@gmail.com>
2022-11-30 22:52:38 -0800

    Add to online.

--- PUZL Boy/Views/Scenes/GameScene.swift
+++ PUZL Boy/Views/Scenes/GameScene.swift
@@ -13,7 +13,7 @@ class GameScene: SKScene {
     
     private var gameEngine: GameEngine
 
-    private var currentLevel: Int = 41 {
+    private var currentLevel: Int = 50 {
         didSet {
             if currentLevel > LevelBuilder.maxLevel {
                 currentLevel = 0
