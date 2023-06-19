aee29fef8b85ce6dafc2bcc3e3c91760ffa7051b
Eddie Char <emchar1@gmail.com>
2023-06-18 18:17:05 -0700

    GameboardSprite: move spacing and scaleSize to local variables. change spritePosition to getSpritePosition(at:) function.

--- PUZL Boy/Views/Sprites/GameboardSprite.swift
+++ PUZL Boy/Views/Sprites/GameboardSprite.swift
@@ -20,7 +20,9 @@ class GameboardSprite {
     static var yPosition: CGFloat { (K.ScreenDimensions.height - K.ScreenDimensions.iPhoneWidth * GameboardSprite.spriteScale - K.ScreenDimensions.topMargin - 283) - (padding / 2) }
 
     typealias WarpTuple = (first: K.GameboardPosition?, second: K.GameboardPosition?)
-
+    
+    private let panelSpacing: CGFloat = 4
+    private var scaleSize: CGSize { CGSize(width: panelSize - panelSpacing, height: panelSize - panelSpacing) }
     private var panels: [[SKSpriteNode]]
     private(set) var panelCount: Int
     private(set) var panelSize: CGFloat
@@ -57,15 +59,16 @@ class GameboardSprite {
     
     // MARK: - Helper Functions
     
+    ///Helper function that takes in the gameboard position and returns where it lies on the screen's CGPoint coordinates.
+    private func getSpritePosition(at position: K.GameboardPosition) -> CGPoint {
+        return CGPoint(x: CGFloat(position.col) * panelSize + panelSpacing / 2,
+                       y: CGFloat(panelCount - 1 - position.row) * panelSize + panelSpacing / 2)
+    }
+
     func updatePanels(at position: K.GameboardPosition, with tile: K.GameboardPanel) {
-        let spacing: CGFloat = 4
-        let scaleSize = CGSize(width: panelSize - spacing, height: panelSize - spacing)
-        let spritePosition = CGPoint(x: CGFloat(position.col) * panelSize + spacing / 2,
-                                     y: CGFloat(panelCount - 1 - position.row) * panelSize + spacing / 2)
-        
         panels[position.row][position.col] = SKSpriteNode(imageNamed: tile.terrain.description)
         panels[position.row][position.col].scale(to: scaleSize)
-        panels[position.row][position.col].position = spritePosition + CGPoint(x: GameboardSprite.padding / 2, y: GameboardSprite.padding / 2)
+        panels[position.row][position.col].position = getSpritePosition(at: position) + CGPoint(x: GameboardSprite.padding / 2, y: GameboardSprite.padding / 2)
         panels[position.row][position.col].anchorPoint = .zero
         panels[position.row][position.col].zPosition = K.ZPosition.terrain
         panels[position.row][position.col].name = "\(position.row),\(position.col)"
@@ -85,7 +88,7 @@ class GameboardSprite {
         if tile.overlay != .boundary {
             let overlayPanel = SKSpriteNode(imageNamed: tile.overlay.description)
             overlayPanel.scale(to: scaleSize)
-            overlayPanel.position = spritePosition
+            overlayPanel.position = getSpritePosition(at: position)
             overlayPanel.anchorPoint = .zero
             overlayPanel.zPosition = K.ZPosition.overlay
             overlayPanel.name = "\(position.row),\(position.col)\(GameboardSprite.overlayTag)"
@@ -177,7 +180,7 @@ class GameboardSprite {
         return CGPoint(x: panelSize * (CGFloat(position.col) + 0.5), y: panelSize * (CGFloat(panelCount - 1 - position.row) + 0.5))
     }
     
-    func colorizeGameboard(color: UIColor, blendFactor: CGFloat, animationDuration: TimeInterval, completion: (() -> ())?) {
+    func colorizeGameboard(color: UIColor, blendFactor: CGFloat, animationDuration: TimeInterval, completion: (() -> Void)?) {
         let colorizeAction = SKAction.colorize(with: color, colorBlendFactor: blendFactor, duration: animationDuration)
 
         for (row, panelRows) in panels.enumerated() {
