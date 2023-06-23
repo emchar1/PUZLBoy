cd911733a41d268801e39f720db6b9a1db177786
Eddie Char <emchar1@gmail.com>
2023-06-22 17:53:51 -0700

    Update partyGemDouble, Triple colors and spread.

--- PUZL Boy/Models/Engines/ChatEngine.swift
+++ PUZL Boy/Models/Engines/ChatEngine.swift
@@ -579,7 +579,7 @@ extension ChatEngine {
             }
             
             sendChat(profile: .trainer, startNewChat: true, endChat: false,
-                     chat: "Congrats! You made it to level 100. There's a bonus at the end of every 50 levels. Beat this and you're one step closer to indescribable fun!!! 🕺🏻") { [unowned self] in
+                     chat: "Congrats! You made it to level 100. There's a bonus at the end of every 50 levels. Beat this and you're one step closer to indescribable fun!!! 💃🏾🪩🕺🏻") { [unowned self] in
                 sendChat(profile: .hero, startNewChat: false, endChat: false, chat: "I can hardly contain my excitement. 😒") { [unowned self] in
                     sendChat(profile: .trainer, startNewChat: false, endChat: false, chat: "That's the spirit! Now if you ever get stuck, you can give your device a shake or tap the red Reset button to restart the level.") { [unowned self] in
                         sendChat(profile: .trainer, startNewChat: false, endChat: false, chat: "Be warned though, restarting a level will cost you one of your precious lives...") { [unowned self] in
