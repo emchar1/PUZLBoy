956dd6f4d25bbf7a88dbc49c85677a73d60977a0
Eddie Char <emchar1@gmail.com>
2023-11-17 01:12:34 -0800

    Update AchievementsList and Levels17MoveList through 301. AchievementSuperEfficient: update requirement to 30. Update build to 1.19 (20)

--- PUZL Boy/Models/Game Center/Achievements/AchievementSuperEfficient.swift
+++ PUZL Boy/Models/Game Center/Achievements/AchievementSuperEfficient.swift
@@ -8,7 +8,7 @@
 import Foundation
 
 class AchievementSuperEfficient: BaseAchievement {
-    let requirement: Double = 22
+    let requirement: Double = 30
     
     override func updatePercentage(increment: Double = 1) {
         percentComplete += increment / requirement * 100
