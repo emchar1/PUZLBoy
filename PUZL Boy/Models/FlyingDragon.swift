8efb58b856e3c7194c2a5c201a55464e0477033d
Eddie Char <emchar1@gmail.com>
2024-01-30 17:56:23 -0800

    Revert Flying Dragon to red, in intro.

--- PUZL Boy/Models/FlyingDragon.swift
+++ PUZL Boy/Models/FlyingDragon.swift
@@ -30,7 +30,7 @@ struct FlyingDragon {
         textures = []
 
         for i in 1...6 {
-            textures.append(atlas.textureNamed("\(FireIceTheme.textureFlyingDragon) (\(i))"))
+            textures.append(atlas.textureNamed("flyingDragon (\(i))"))
         }
         
         sprite = SKSpriteNode(texture: textures[0])
