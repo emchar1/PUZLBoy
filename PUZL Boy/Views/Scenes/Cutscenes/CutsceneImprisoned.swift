8886e60179a802ae74377652ea9bfb2dff8cfde3
Eddie Char <emchar1@gmail.com>
2024-03-05 12:52:21 -0800

    Add CutsceneImprisoned (Princess in birdcage) and CutsceneTransformation (Princess evolution + Magmoor defeat) classes. Rearrange files so folders appear on top.

---
+++ PUZL Boy/Views/Scenes/Cutscenes/CutsceneImprisoned.swift
@@ -0,0 +1,33 @@
+//
+//  CutsceneImprisoned.swift
+//  PUZL Boy
+//
+//  Created by Eddie Char on 3/4/24.
+//
+
+import SpriteKit
+
+class CutsceneImprisoned: Cutscene {
+    
+    // MARK: - Properties
+    
+    
+    // MARK: - Initialization
+    
+    override func setupScene() {
+        super.setupScene()
+        
+        //Custom implementation.
+    }
+    
+    override func cleanupScene(buttonTap: ButtonTap.ButtonType?, fadeDuration: TimeInterval?) {
+        super.cleanupScene(buttonTap: buttonTap, fadeDuration: fadeDuration)
+        
+        //Custom implementation here, if needed.
+    }
+    
+    
+    // MARK: - Functions
+    
+    
+}
---
+++ PUZL Boy/Views/Scenes/Cutscenes/CutsceneTransformation.swift
@@ -0,0 +1,33 @@
+//
+//  CutsceneTransformation.swift
+//  PUZL Boy
+//
+//  Created by Eddie Char on 3/4/24.
+//
+
+import SpriteKit
+
+class CutsceneTransformation: Cutscene {
+    
+    // MARK: - Properties
+    
+    
+    // MARK: - Initialization
+    
+    override func setupScene() {
+        super.setupScene()
+        
+        //Custom implementation.
+    }
+    
+    override func cleanupScene(buttonTap: ButtonTap.ButtonType?, fadeDuration: TimeInterval?) {
+        super.cleanupScene(buttonTap: buttonTap, fadeDuration: fadeDuration)
+        
+        //Custom implementation here, if needed.
+    }
+    
+    
+    // MARK: - Functions
+    
+    
+}
