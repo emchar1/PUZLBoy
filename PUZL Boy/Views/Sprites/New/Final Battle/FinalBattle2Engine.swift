271c1404d4f8a987260b597246136f075ca763ad
Eddie Char <emchar1@gmail.com>
2024-12-16 01:14:11 -0800

    FinalBattle2Engine: add this class.

---
+++ PUZL Boy/Views/Sprites/New/Final Battle/FinalBattle2Engine.swift
@@ -0,0 +1,32 @@
+//
+//  FinalBattle2Engine.swift
+//  PUZL Boy
+//
+//  Created by Eddie Char on 12/15/24.
+//
+
+class FinalBattle2Engine {
+    
+    // MARK: - Properties
+    
+    private var gameboardSprite: GameboardSprite!
+    private var playerSprite: PlayerSprite!
+    
+    
+    // MARK: - Initialization
+    
+    init() {
+        
+    }
+    
+    deinit {
+        print("FinalBattle2Engine deinit")
+    }
+    
+    
+    // MARK: - Functions
+    
+    
+    
+    
+}
