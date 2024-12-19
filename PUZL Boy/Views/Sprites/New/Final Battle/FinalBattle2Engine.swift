e2ac5294f6d215841860ad180c598ce60a58858b
Eddie Char <emchar1@gmail.com>
2024-12-19 10:05:24 -0800

    FinalBattle2Engine: Build this out. FinalBattleScene: Build this out.

--- PUZL Boy/Views/Sprites/New/Final Battle/FinalBattle2Engine.swift
+++ PUZL Boy/Views/Sprites/New/Final Battle/FinalBattle2Engine.swift
@@ -5,28 +5,113 @@
 //  Created by Eddie Char on 12/15/24.
 //
 
+import SpriteKit
+
 class FinalBattle2Engine {
     
     // MARK: - Properties
     
-    private var gameboardSprite: GameboardSprite!
-    private var playerSprite: PlayerSprite!
+    private(set) var gameEngine: GameEngine!
+
+    private let startPosition: K.GameboardPosition = (6, 3)
+    private let ignorePositions: [K.GameboardPosition] = [(3, 3), (6, 3)]
+    private let maxCount: Int = 1000
+
+    private var spawnPanels0: [K.GameboardPosition] = []
+    private var spawnPanels1: [K.GameboardPosition] = []
+    private var spawnPanels2: [K.GameboardPosition] = []
     
     
     // MARK: - Initialization
     
     init() {
-        
+        setupScene()
     }
     
     deinit {
         print("FinalBattle2Engine deinit")
     }
     
+    private func setupScene() {
+        gameEngine = GameEngine(level: Level.finalLevel + 1, shouldSpawn: true)
+        
+        //Make sure to initialize GameboardSprite BEFORE initializing these!!!
+        populateSpawnPanels(spawnPanels: &spawnPanels0, startPosition: startPosition, ignorePositions: ignorePositions, maxCount: maxCount)
+        populateSpawnPanels(spawnPanels: &spawnPanels1, startPosition: startPosition, ignorePositions: ignorePositions, maxCount: maxCount)
+        populateSpawnPanels(spawnPanels: &spawnPanels2, startPosition: startPosition, ignorePositions: ignorePositions, maxCount: maxCount)
+        
+        animatePanels(spawnPanels: spawnPanels0, with: .start)
+//        animatePanels(spawnPanels: spawnPanels1, with: .start)
+//        animatePanels(spawnPanels: spawnPanels2, with: .start)
+    }
+    
+    
+    // MARK: - Move Functions
+    
+    /**
+     Adds all the sprites to the superScene, i.e. should be called in a GameScene's moveTo() function.
+     - parameter superScene: The GameScene to add all the children to.
+     */
+    func moveSprites(to superScene: SKScene) {
+        gameEngine.moveSprites(to: superScene)
+    }
+    
     
     // MARK: - Functions
     
+    func populateSpawnPanels(spawnPanels: inout [K.GameboardPosition], startPosition: K.GameboardPosition, ignorePositions: [K.GameboardPosition] = [], count: Int = 0, maxCount: Int = 100) {
+        
+        //Base case
+        guard count < maxCount else { return }
+        
+        let nextPosition = spawnNextPosition(startPosition: startPosition, ignorePositions: ignorePositions)
+        spawnPanels.append(nextPosition)
+        
+        let spawnPanelsToIgnore = spawnPanels.count >= 2 ? Array(spawnPanels.suffix(2)) : []
+                
+        //Recursion!
+        populateSpawnPanels(spawnPanels: &spawnPanels,
+                            startPosition: nextPosition,
+                            ignorePositions: self.ignorePositions + spawnPanelsToIgnore, //must be class var, ignorePositions
+                            count: count + 1,
+                            maxCount: maxCount)
+    }
     
+    private func spawnNextPosition(startPosition: K.GameboardPosition, ignorePositions: [K.GameboardPosition]) -> K.GameboardPosition {
+        var nextPosition: K.GameboardPosition
+        
+        repeat {
+            let spawnCol = Bool.random()
+            let spawnOffset = Bool.random() ? -1 : 1
+            
+            nextPosition = (startPosition.row + (spawnCol ? 0 : spawnOffset), startPosition.col + (spawnCol ? spawnOffset : 0))
+        } while nextPosition.row < 0 || nextPosition.row >= gameEngine.gameboardSprite.panelCount || nextPosition.col < 0 || nextPosition.col >= gameEngine.gameboardSprite.panelCount || ignorePositions.contains { $0.row == nextPosition.row && $0.col == nextPosition.col }
+        
+        return nextPosition
+    }
+    
+    // TODO: - WIP
+    private func animatePanels(spawnPanels: [K.GameboardPosition], with terrain: LevelType) {
+        for (i, spawnPanel) in spawnPanels.enumerated() {
+            guard let originalTerrain = gameEngine.gameboardSprite.getPanelSprite(at: spawnPanel).terrain else { continue }
+
+            let waitDuration: TimeInterval = 0.1
+            let newTerrain = SKSpriteNode(imageNamed: terrain.description)
+
+            newTerrain.anchorPoint = .zero
+            newTerrain.alpha = 0
+            newTerrain.zPosition = 1
+            originalTerrain.addChild(newTerrain)
+            
+            newTerrain.run(SKAction.sequence([
+                SKAction.wait(forDuration: waitDuration * TimeInterval(i)),
+                SKAction.fadeIn(withDuration: 0),
+                SKAction.wait(forDuration: waitDuration),
+                SKAction.fadeOut(withDuration: waitDuration * 10),
+                SKAction.removeFromParent()
+            ]))
+        }
+    }
     
     
 }
--- PUZL Boy/Views/Sprites/New/Final Battle/FinalBattleScene.swift
+++ PUZL Boy/Views/Sprites/New/Final Battle/FinalBattleScene.swift
@@ -11,15 +11,9 @@ class FinalBattleScene: SKScene {
     
     // MARK: - Properties
     
-    private var hero: Player!
-    private var elder0: Player!
-    private var elder1: Player!
-    private var elder2: Player!
-    private var princess: Player!
-    private var villain: Player!
-
-    private var gameboardSprite: GameboardSprite!
     private var backgroundSprite: SKSpriteNode!
+    private var finalBattle2Engine: FinalBattle2Engine!
+    private var villain: Player!
 
     
     // MARK: - Initialization
@@ -39,73 +33,111 @@ class FinalBattleScene: SKScene {
         
         AudioManager.shared.stopSound(for: "bossbattle1", fadeDuration: 2)
         AudioManager.shared.stopSound(for: "bossbattle2", fadeDuration: 2)
+        AudioManager.shared.stopSound(for: "bossbattle3", fadeDuration: 2)
     }
     
     private func setupScene() {
         backgroundColor = .black
 
-        gameboardSprite = GameboardSprite(level: LevelBuilder.levels[Level.finalLevel + 1], fadeIn: false)
+        finalBattle2Engine = FinalBattle2Engine()
         
         //These should go AFTER initializing gameboardSprite.
-        let gameboardSpriteSize: CGSize = gameboardSprite.sprite.size / gameboardSprite.sprite.xScale
-        let gameboardSpritePosition: CGPoint = gameboardSprite.sprite.position
-        let gameboardSpriteScale: CGFloat = gameboardSprite.sprite.xScale
-        let playerScale: CGFloat = Player.getGameboardScale(panelSize: size.width / CGFloat(gameboardSprite.panelCount))
+        let gameboardSpriteSize: CGSize = finalBattle2Engine.gameEngine.gameboardSprite.sprite.size / finalBattle2Engine.gameEngine.gameboardSprite.sprite.xScale
+        let gameboardSpritePosition: CGPoint = finalBattle2Engine.gameEngine.gameboardSprite.sprite.position
+        let gameboardSpriteScale: CGFloat = finalBattle2Engine.gameEngine.gameboardSprite.sprite.xScale
+        let playerScale: CGFloat = Player.getGameboardScale(panelSize: size.width / CGFloat(finalBattle2Engine.gameEngine.gameboardSprite.panelCount))
 
-        backgroundSprite = SKSpriteNode(color: .green, size: gameboardSpriteSize)
+        backgroundSprite = SKSpriteNode(color: .clear, size: gameboardSpriteSize)
         backgroundSprite.position = gameboardSpritePosition
         backgroundSprite.setScale(gameboardSpriteScale)
         backgroundSprite.anchorPoint = .zero
-        backgroundSprite.alpha = 0.5
-        backgroundSprite.zPosition = gameboardSprite.sprite.zPosition + K.ZPosition.overlay + 5
+        backgroundSprite.alpha = 1
+        backgroundSprite.zPosition = finalBattle2Engine.gameEngine.gameboardSprite.sprite.zPosition + K.ZPosition.overlay + 5
         
-        hero = Player(type: .hero)
-        hero.sprite.position = gameboardSprite.getLocation(at: (6, 3))
-        hero.sprite.setScale(playerScale * hero.scaleMultiplier)
-        hero.sprite.zPosition = K.ZPosition.player
-
-        elder0 = Player(type: .elder0)
-        elder0.sprite.position = hero.sprite.position + CGPoint(x: -gameboardSprite.panelSize, y: 50)
-        elder0.sprite.setScale(playerScale * elder0.scaleMultiplier)
-        elder0.sprite.zPosition = K.ZPosition.player - 5
-
-        elder1 = Player(type: .elder1)
-        elder1.sprite.position = hero.sprite.position + CGPoint(x: gameboardSprite.panelSize, y: 50)
-        elder1.sprite.setScale(playerScale * elder1.scaleMultiplier)
-        elder1.sprite.zPosition = K.ZPosition.player - 10
-
-        elder2 = Player(type: .elder2)
-        elder2.sprite.position = hero.sprite.position + CGPoint(x: 0, y: -gameboardSprite.panelSize)
-        elder2.sprite.setScale(playerScale * elder2.scaleMultiplier)
-        elder2.sprite.zPosition = K.ZPosition.player + 5
+        villain = Player(type: .villain)
+        villain.sprite.position = finalBattle2Engine.gameEngine.gameboardSprite.getLocation(at: (3, 3)) + CGPoint(x: 0, y: 50)
+        villain.sprite.setScale(playerScale * villain.scaleMultiplier)
+        villain.sprite.zPosition = K.ZPosition.player
     }
     
     
     // MARK: - Functions
     
     override func didMove(to view: SKView) {
-        addChild(gameboardSprite.sprite)
         addChild(backgroundSprite)
+        finalBattle2Engine.moveSprites(to: self)
         
-        backgroundSprite.addChild(hero.sprite)
-        backgroundSprite.addChild(elder0.sprite)
-        backgroundSprite.addChild(elder1.sprite)
-        backgroundSprite.addChild(elder2.sprite)
+        backgroundSprite.addChild(villain.sprite)
     }
     
     
     // MARK: - Other Functions
     
     func animateScene() {
-        let bossbattle1Duration = AudioManager.shared.getAudioItem(filename: "bossbattle1")?.player.duration ?? 0
-        AudioManager.shared.playSound(for: "bossbattle1")
-        AudioManager.shared.playSound(for: "bossbattle2", delay: bossbattle1Duration)
+//        let bossbattle1Duration = AudioManager.shared.getAudioItem(filename: "bossbattle1")?.player.duration ?? 0
+//        AudioManager.shared.playSound(for: "bossbattle1")
+//        AudioManager.shared.playSound(for: "bossbattle2", delay: bossbattle1Duration)
+        AudioManager.shared.playSound(for: "bossbattle3")
         
-        hero.sprite.run(Player.animate(player: hero, type: .idle))
-        elder0.sprite.run(Player.animateIdleLevitate(player: elder0))
-        elder1.sprite.run(Player.animateIdleLevitate(player: elder1))
-        elder2.sprite.run(Player.animateIdleLevitate(player: elder2))
+        villain.sprite.run(Player.animateIdleLevitate(player: villain))
         
-//        backgroundSprite.run(SKAction.colorize(with: .clear, colorBlendFactor: 1, duration: 4))
+//        let pulseDuration: TimeInterval = 0.05
+        
+        
+        
+        
+        
+        
+//        for (i, node) in gameboardSprite.sprite.children.enumerated() {
+//            guard let node = node as? SKSpriteNode else { return }
+//            
+//            print(node.name)
+//            
+//            node.run(SKAction.repeatForever(SKAction.sequence([
+//                SKAction.wait(forDuration: Double(i) * pulseDuration),
+//                SKAction.setTexture(SKTexture(imageNamed: "lava")),
+//                SKAction.wait(forDuration: pulseDuration),
+//                SKAction.setTexture(SKTexture(imageNamed: "water")),
+//                SKAction.wait(forDuration: pulseDuration - Double(i) * pulseDuration),
+//            ])))
+//        }
+        
+        
+        
+        
+        
+//        for row in 0..<gameboardSprite.panelCount {
+//            for col in 0..<gameboardSprite.panelCount {
+//                let panelSprite = gameboardSprite.getPanelSprite(at: (row, col))
+//                guard let panel = gameboardSprite.getPanel(at: (row, col)) else { return }
+//                
+//                guard let terrain = panelSprite.terrain else { return }
+//                
+//                print("row: \(row), col: \(col): \(panel.terrain.description), \(panel.overlay.description)")
+//                
+//                terrain.run(SKAction.repeatForever(SKAction.sequence([
+//                    SKAction.wait(forDuration: TimeInterval(row * col) * 0.1),
+//                    SKAction.colorize(with: .black, colorBlendFactor: 1, duration: pulseDuration),
+//                    SKAction.setTexture(SKTexture(imageNamed: "lava")),
+////                    SKAction.run {
+////                        self.gameboardSprite.updatePanels(at: (row, col), with: (.lava, .enemy))
+////                    },
+//                    SKAction.colorize(withColorBlendFactor: 0, duration: pulseDuration),
+//                    SKAction.colorize(withColorBlendFactor: 1, duration: pulseDuration),
+//                    SKAction.setTexture(terrain.texture ?? SKTexture()),
+////                    SKAction.run {
+////                        self.gameboardSprite.updatePanels(at: (row, col), with: (panel.terrain, panel.overlay))
+////                    },
+//                    SKAction.colorize(withColorBlendFactor: 0, duration: pulseDuration),
+//                ])))
+//            }
+//        }
+        
+        
+        
+        
+        
+        
+        
     }
 }
