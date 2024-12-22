04051798e16b8a48ee4400551613e6f3be61dd9e
Eddie Char <emchar1@gmail.com>
2024-12-21 21:27:11 -0800

    FinalBattle2Controls: add this class. FinalBattle2Engine: update to use new FinalBattle2Controls and remove manual implementation of Controls. Add handleControls(); FinalBattleScene now calls this function instead of calling it directly from FinalBattle2Controls. Add endPosition property.

---
+++ PUZL Boy/Views/Sprites/New/Final Battle/FinalBattle2Controls.swift
@@ -0,0 +1,164 @@
+//
+//  FinalBattle2Controls.swift
+//  PUZL Boy
+//
+//  Created by Eddie Char on 12/21/24.
+//
+
+import SpriteKit
+
+class FinalBattle2Controls {
+    
+    // MARK: - Properties
+    
+    private var gameboard: GameboardSprite
+    private var player: Player
+    private var playerPosition: K.GameboardPosition
+    
+    private var location: CGPoint!
+    private var isDisabled: Bool
+
+    
+    // MARK: - Initialization
+    
+    init(gameboard: GameboardSprite, player: Player, playerPosition: K.GameboardPosition) {
+        self.gameboard = gameboard
+        self.player = player
+        self.playerPosition = playerPosition
+        
+        self.isDisabled = false
+    }
+    
+    deinit {
+        print("deinit FinalBattle2Controls")
+    }
+    
+    
+    // MARK: - Functions
+    
+    /**
+     Handles player movement based on control input.
+     - parameter location: Location for which comparison is to occur
+     */
+    func handleControls(in location: CGPoint, playerPosition: inout K.GameboardPosition) {
+        guard !isDisabled else { return }
+        
+        self.location = location
+        
+        if inBounds(.up) {
+            movePlayerHelper(.up)
+        }
+        else if inBounds(.down) {
+            movePlayerHelper(.down)
+        }
+        else if inBounds(.left) {
+            movePlayerHelper(.left)
+        }
+        else if inBounds(.right) {
+            movePlayerHelper(.right)
+        }
+        else {
+            //handle default cases here...
+        }
+        
+        //Since argument is inout, remember to set the global variable, self.playerPosition to the playerPosition argument! Put this line last!!!
+        playerPosition = self.playerPosition
+        
+        print("playerPosition: \(playerPosition), point: \(player.sprite.position)")
+    }
+    
+    
+    // MARK: - Controls Helper Functions
+    
+    /**
+     Takes a tap location and compares it to the player's next position.
+     - parameter direction: The player's next position, either up, down, left, or right
+     - returns: true or false, depending on if the requested direction of movement is within the gameboard bounds
+     */
+    private func inBounds(_ direction: Controls) -> Bool {
+        let maxDistance: CGFloat = CGFloat(gameboard.panelCount)
+        let panelSize: CGFloat = gameboard.panelSize * UIDevice.spriteScale
+        let gameboardSize: CGFloat = panelSize * CGFloat(maxDistance)
+        let gameboardOffset: CGPoint = GameboardSprite.offsetPosition
+        
+        var bottomBound: CGFloat = CGFloat(playerPosition.row) + 1
+        var rightBound: CGFloat = CGFloat(playerPosition.col) + 1
+        var topBound: CGFloat = CGFloat(playerPosition.row) {
+            didSet { topBound = max(0, topBound) }
+        }
+        var leftBound: CGFloat = CGFloat(playerPosition.col) {
+            didSet { leftBound = max(0, leftBound) }
+        }
+        
+        switch direction {
+        case .up:
+            topBound = -maxDistance
+            bottomBound -= 1
+        case .down:
+            topBound += 1
+            bottomBound = maxDistance
+        case .left:
+            leftBound = -maxDistance
+            rightBound -= 1
+        case .right:
+            leftBound += 1
+            rightBound = maxDistance
+        default:
+            print("Unknown direction in FinalBattle2Controls.inBounds()")
+        }
+        
+        //Finally...
+        let locationInsideLeftBound = location.x > gameboardOffset.x + leftBound * panelSize
+        let locationInsideRightBound = location.x < gameboardOffset.x + rightBound * panelSize
+        let locationInsideBottomBound = location.y > gameboardOffset.y + gameboardSize - bottomBound * panelSize
+        let locationInsideTopBound = location.y < gameboardOffset.y + gameboardSize - topBound * panelSize
+        
+        return locationInsideLeftBound && locationInsideRightBound && locationInsideBottomBound && locationInsideTopBound
+    }
+    
+    private func isValidMove(_ direction: Controls) -> Bool {
+        // TODO: - Check if move intersects with villain or any other unforseen obstacle...
+        return true
+    }
+    
+    /**
+     Physically move the player in the intended direction.
+     - parameter direction: The direction the player would like to move to
+     */
+    private func movePlayerHelper(_ direction: Controls) {
+        var nextPanel: K.GameboardPosition
+        
+        switch direction {
+        case .up:
+            nextPanel = (row: playerPosition.row - 1, col: playerPosition.col)
+        case .down:
+            nextPanel = (row: playerPosition.row + 1, col: playerPosition.col)
+        case .left:
+            nextPanel = (row: playerPosition.row, col: playerPosition.col - 1)
+            player.sprite.xScale = -abs(player.sprite.xScale)
+        case .right:
+            nextPanel = (row: playerPosition.row, col: playerPosition.col + 1)
+            player.sprite.xScale = abs(player.sprite.xScale)
+        default:
+            nextPanel = (row: playerPosition.row, col: playerPosition.col)
+        }
+        
+        let runSound = "movetile\(Int.random(in: 1...3))"
+        
+        playerPosition = nextPanel
+        isDisabled = true
+        
+        AudioManager.shared.playSound(for: runSound)
+        
+        player.sprite.run(Player.animate(player: player, type: .run))
+        player.sprite.run(SKAction.sequence([
+            SKAction.wait(forDuration: Player.Texture.run.movementSpeed),
+            Player.animate(player: player, type: .idle)
+        ]))
+        player.sprite.run(SKAction.move(to: gameboard.getLocation(at: nextPanel), duration: Player.Texture.run.movementSpeed)) { [weak self] in
+            self?.isDisabled = false
+            
+            AudioManager.shared.stopSound(for: runSound, fadeDuration: 0.25)
+        }
+    }
+}
--- PUZL Boy/Views/Sprites/New/Final Battle/FinalBattle2Engine.swift
+++ PUZL Boy/Views/Sprites/New/Final Battle/FinalBattle2Engine.swift
@@ -15,18 +15,18 @@ class FinalBattle2Engine {
     private let maxCount: Int = 1000
     private let ignorePositions: [K.GameboardPosition] = [(3, 3), (6, 3)]
     private let startPosition: K.GameboardPosition = (6, 3)
-    private var villainPosition: K.GameboardPosition = (3, 3)
+    private let endPosition: K.GameboardPosition = (3, 3)
     private var heroPosition: K.GameboardPosition!
+    private var villainPosition: K.GameboardPosition!
     
     private var spawnPanels0: [K.GameboardPosition] = []
     private var spawnPanels1: [K.GameboardPosition] = []
     private var spawnPanels2: [K.GameboardPosition] = []
     
-    private var isDisabled: Bool = false
-    
     private(set) var gameboard: GameboardSprite!
     private(set) var hero: Player!
     private(set) var villain: Player!
+    private(set) var controls: FinalBattle2Controls!
     
     
     // MARK: - Initialization
@@ -43,6 +43,7 @@ class FinalBattle2Engine {
     
     private func setupScene() {
         heroPosition = startPosition
+        villainPosition = endPosition
         gameboard = GameboardSprite(level: LevelBuilder.levels[Level.finalLevel + 1], fadeIn: false)
         
         //Make sure to initialize GameboardSprite BEFORE initializing these!!!
@@ -58,6 +59,9 @@ class FinalBattle2Engine {
         villain.sprite.setScale(playerScale * villain.scaleMultiplier)
         villain.sprite.xScale *= -1
         villain.sprite.zPosition = K.ZPosition.player - 2
+        
+        //Initialize after gameboard, hero and heroPosition!
+        controls = FinalBattle2Controls(gameboard: gameboard, player: hero, playerPosition: heroPosition)
         
         populateSpawnPanels(spawnPanels: &spawnPanels0, startPosition: heroPosition, ignorePositions: ignorePositions, maxCount: maxCount)
         populateSpawnPanels(spawnPanels: &spawnPanels1, startPosition: heroPosition, ignorePositions: ignorePositions, maxCount: maxCount)
@@ -87,6 +91,10 @@ class FinalBattle2Engine {
         return nodes.contains(where: { $0.name == "safePanel" || $0.name == startTerrain })
     }
     
+    func handleControls(in location: CGPoint) {
+        controls.handleControls(in: location, playerPosition: &heroPosition)
+    }
+    
     ///Animates all the components
     func animateSprites() {
         hero.sprite.run(Player.animate(player: hero, type: .idle))
@@ -95,123 +103,6 @@ class FinalBattle2Engine {
         animateSpawnPanels(spawnPanels: spawnPanels0, with: .start)
 //        animateSpawnPanels(spawnPanels: spawnPanels1, with: .start)
 //        animateSpawnPanels(spawnPanels: spawnPanels2, with: .start)
-    }
-    
-    
-    // MARK: - Controls Functions
-    
-    /**
-     Handles player movement based on control input.
-     - parameter location: Location for which comparison is to occur.
-     */
-    func handleControls(in location: CGPoint) {
-        guard !isDisabled else { return }
-        
-        if inBounds(location: location, direction: .up) {
-            movePlayerHelper(direction: .up)
-        }
-        else if inBounds(location: location, direction: .down) {
-            movePlayerHelper(direction: .down)
-        }
-        else if inBounds(location: location, direction: .left) {
-            hero.sprite.xScale = -abs(hero.sprite.xScale)
-            
-            movePlayerHelper(direction: .left)
-        }
-        else if inBounds(location: location, direction: .right) {
-            hero.sprite.xScale = abs(hero.sprite.xScale)
-            
-            movePlayerHelper(direction: .right)
-        }
-        else {
-            //handle default cases here...
-        }
-    }
-    
-    
-    // MARK: - Controls Helper Functions
-    
-    /**
-     Takes a tap location and compares it to the player's next position.
-     - parameters:
-        - location: Location of the tap
-        - controls: The player's next position, either up, down, left, or right
-     */
-    private func inBounds(location: CGPoint, direction: Controls) -> Bool {
-        let maxDistance = gameboard.panelCount
-        let panelSize = gameboard.panelSize * UIDevice.spriteScale
-        let gameboardSize = panelSize * CGFloat(maxDistance)
-        
-        var bottomBound = heroPosition.row + 1
-        var rightBound = heroPosition.col + 1
-        var topBound = heroPosition.row {
-            didSet {
-                topBound = max(0, topBound)
-            }
-        }
-        var leftBound = heroPosition.col {
-            didSet {
-                leftBound = max(0, leftBound)
-            }
-        }
-        
-        switch direction {
-        case .up:
-            topBound = -maxDistance
-            bottomBound -= 1
-        case .down:
-            topBound += 1
-            bottomBound = maxDistance
-        case .left:
-            leftBound = -maxDistance
-            rightBound -= 1
-        case .right:
-            leftBound += 1
-            rightBound = maxDistance
-        default:
-            print("Unknown direction in FinalBattle2Engine.inBounds()")
-        }
-        
-        let locationInsideLeftBound = location.x > GameboardSprite.offsetPosition.x + (CGFloat(leftBound) * panelSize)
-        let locationInsideRightBound = location.x < GameboardSprite.offsetPosition.x + (CGFloat(rightBound) * panelSize)
-        let locationInsideBottomBound = location.y > GameboardSprite.offsetPosition.y + gameboardSize - (CGFloat(bottomBound) * panelSize)
-        let locationInsideTopBound = location.y < GameboardSprite.offsetPosition.y + gameboardSize - (CGFloat(topBound) * panelSize)
-        
-        return locationInsideLeftBound && locationInsideRightBound && locationInsideBottomBound && locationInsideTopBound
-    }
-    
-    /**
-     Helper function that moves the player.
-     - parameter direction: The direction the player is moving
-     */
-    private func movePlayerHelper(direction: Controls) {
-        var nextPanel: K.GameboardPosition
-        
-        switch direction {
-        case .up:       nextPanel = (row: heroPosition.row - 1, col: heroPosition.col)
-        case .down:     nextPanel = (row: heroPosition.row + 1, col: heroPosition.col)
-        case .left:     nextPanel = (row: heroPosition.row, col: heroPosition.col - 1)
-        case .right:    nextPanel = (row: heroPosition.row, col: heroPosition.col + 1)
-        default:        nextPanel = (row: heroPosition.row, col: heroPosition.col)
-        }
-        
-        let runSound = "movetile\(Int.random(in: 1...3))"
-        
-        heroPosition = nextPanel
-        isDisabled = true
-        
-        AudioManager.shared.playSound(for: runSound)
-        
-        hero.sprite.run(Player.animate(player: hero, type: .run))
-        hero.sprite.run(SKAction.sequence([
-            SKAction.wait(forDuration: Player.Texture.run.movementSpeed),
-            Player.animate(player: hero, type: .idle)
-        ]))
-        hero.sprite.run(SKAction.move(to: gameboard.getLocation(at: nextPanel), duration: Player.Texture.run.movementSpeed)) { [weak self] in
-            self?.isDisabled = false
-            
-            AudioManager.shared.stopSound(for: runSound, fadeDuration: 0.25)
-        }
     }
     
     
