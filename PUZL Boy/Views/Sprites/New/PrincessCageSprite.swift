151fb719339f66a1dd55982fb658f7aa7b0ee315
Eddie Char <emchar1@gmail.com>
2025-01-06 11:21:28 -0800

    PrincessCageSprite: refactor again. Princess wait before writheAction.

--- PUZL Boy/Views/Sprites/New/PrincessCageSprite.swift
+++ PUZL Boy/Views/Sprites/New/PrincessCageSprite.swift
@@ -70,15 +70,23 @@ class PrincessCageSprite: SKNode {
     // MARK: - Functions
     
     func encagePrincess() {
+        //Properties
+        let villain = Player(type: .villain)
+        let princess = Player(type: .princess)
         let attackDuration: TimeInterval = 0.12 * 3
         let flingDuration: TimeInterval = 0.5
         let fadeDuration: TimeInterval = 1.8
         
-        let villain = Player(type: .villain)
+        //Actions
+        let waitAction = SKAction.wait(forDuration: flingDuration + attackDuration)
         let attackAction = SKAction.animate(with: villain.textures[Player.Texture.attack.rawValue], timePerFrame: 0.12)
-        
-        let princess = Player(type: .princess)
         let writheAction = Player.animate(player: princess, type: .jump)
+        let rotateAction = SKAction.repeatForever(SKAction.rotate(byAngle: .pi / 4, duration: fadeDuration))
+        
+        let pulseAction = SKAction.repeatForever(SKAction.sequence([
+            SKAction.scale(to: scaleSize * 0.9, duration: fadeDuration),
+            SKAction.scale(to: scaleSize * 1.1, duration: fadeDuration)
+        ]))
         
         let flingAction = SKAction.sequence([
             SKAction.wait(forDuration: attackDuration),
@@ -94,41 +102,33 @@ class PrincessCageSprite: SKNode {
             SKAction.removeFromParent()
         ])
         
-        let rotateAction = SKAction.repeatForever(SKAction.rotate(byAngle: .pi / 4, duration: fadeDuration))
-        
-        let pulseAction = SKAction.repeatForever(SKAction.sequence([
-            SKAction.scale(to: scaleSize * 0.9, duration: fadeDuration),
-            SKAction.scale(to: scaleSize * 1.1, duration: fadeDuration)
-        ]))
-        
-        let encageAction = SKAction.sequence([
-            SKAction.wait(forDuration: flingDuration + attackDuration),
-            SKAction.group([
-                SKAction.fadeIn(withDuration: fadeDuration / 3),
-                SKAction.colorize(with: .red, colorBlendFactor: 1, duration: fadeDuration),
-                SKAction.sequence([
-                    SKAction.scale(to: scaleSize * 0.50, duration: fadeDuration / 6),
-                    SKAction.scale(to: scaleSize * 2.00, duration: fadeDuration / 5),
-                    SKAction.scale(to: scaleSize * 0.75, duration: fadeDuration / 4),
-                    SKAction.scale(to: scaleSize * 1.25, duration: fadeDuration / 3),
-                    SKAction.scale(to: scaleSize * 0.90, duration: fadeDuration / 2),
-                    SKAction.scale(to: scaleSize * 1.10, duration: fadeDuration / 1)
-                ])
+        let encageAction = SKAction.group([
+            SKAction.fadeIn(withDuration: fadeDuration / 3),
+            SKAction.colorize(with: .red, colorBlendFactor: 1, duration: fadeDuration),
+            SKAction.sequence([
+                SKAction.scale(to: scaleSize * 0.50, duration: fadeDuration / 6),
+                SKAction.scale(to: scaleSize * 2.00, duration: fadeDuration / 5),
+                SKAction.scale(to: scaleSize * 0.75, duration: fadeDuration / 4),
+                SKAction.scale(to: scaleSize * 1.25, duration: fadeDuration / 3),
+                SKAction.scale(to: scaleSize * 0.90, duration: fadeDuration / 2),
+                SKAction.scale(to: scaleSize * 1.10, duration: fadeDuration / 1)
             ])
         ])
         
+        
+        //Animations & Sound
         villainNode.run(attackAction)
         
         princessNode.removeAllActions()
-        princessNode.run(writheAction)
+        princessNode.run(SKAction.sequence([waitAction, writheAction]))
         
         flingNode.run(flingAction)
         
         backNode.run(rotateAction)
-        backNode.run(SKAction.sequence([encageAction, pulseAction]))
+        backNode.run(SKAction.sequence([waitAction, encageAction, pulseAction]))
         
         frontNode.run(rotateAction)
-        frontNode.run(SKAction.sequence([encageAction, pulseAction]))
+        frontNode.run(SKAction.sequence([waitAction, encageAction, pulseAction]))
         
         AudioManager.shared.playSound(for: "shieldcast", delay: flingDuration + attackDuration)
         AudioManager.shared.playSound(for: "shieldcast2", delay: flingDuration + attackDuration)
